c266bdc47ccbaf2ad645c5606ea500ba43973516
nson7 <nson7@sina.com>
2016-03-29 11:18:34 +0800

    mark.c file ------ nson7

---
+++ mark.c
@@ -0,0 +1,8 @@
+          ,aw:swap window
+          ,ii:function
+          ,sc:shell
+          ,bb:buffer
+          ,vv:other window down
+          ,x0:delete-window
+          ,xm:M-x
+               ,ov:overview
