818adb1d97dce19125b4d74a58e7e8ec91b0243e
nson7 <nson7@sina.com>
2016-06-02 11:27:59 +0800

    init.el-6-2 ------ nson7

---
+++ init.el
@@ -0,0 +1,18 @@
+
+(setq frame-title-format "%b")
+(tool-bar-mode -1)
+(scroll-bar-mode t)
+(menu-bar-mode -1)
+
+(setq inhibit-startup-message t)
+(setq inhibit-splash-screen t)
+(global-linum-mode t)
+
+(setq column-number-mode t)
+(setq line-number-mode t)
+(blink-cursor-mode -1)
+(set-background-color "#CCFFCC")
+
+;;不产生备份文件 
+(setq make-backup-files nil)
+
