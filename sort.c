6992f86261251e36273479c0ca22422f3329f1f6
nson7 <nson7@sina.com>
2016-09-05 16:30:33 +0800

    modified sort.c callback

--- sort.c
+++ sort.c
@@ -1,94 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
-void quick_sort(int *a, int left, int right);
-void bubble_sort(int *a, int count);
-void insert_sort(int*array, unsigned int n);
-void select_sort(int *array, unsigned int n);
-
-int main()
+void swap(int *a, int *b)
 {
-    int i;
-    int k[] = {9, 7, 7, 5, 3, 2};
+    int temp;
 
-    select_sort(k, 6);
-
-    for (i = 0; i < 6; i++)
-        printf("%d ", k[i]);
-    printf("\n");
-
-    exit(0);
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void quick_sort(int *a, int left, int right)
-{
-    if (left >= right)
-    {
-        return ;
-    }
-
-    int n;
-    int i = left;
-    int j = right;
-    int key = a[left];
-
-    while (i < j)
-    {
-        while ((i < j) && (key <= a[j]))
-        {
-            j--;
-        }
-
-        a[i] = a[j];
-        while ((i < j) && (key >= a[i]))
-        {
-            i++;
-        }
-
-        a[j] = a[i];
-    }
-
-    a[i] = key;
-    quick_sort(a, left, i - 1);
-    quick_sort(a, i + 1, right);
-}
-
-void bubble_sort(int *a, int count)
-{
-    int i;
-    int j;
-
-    for (j = 1; j < count; j++)
-    {
-        for (i = 0; i < count - j; i++)
-        {
-            if (a[i] > a[i+1])
-            {
-                a[i] ^= a[i+1];
-                a[i+1] ^= a[i];
-                a[i] ^= a[i+1];
-            }
-        }
-    }
-
-    return ;
-}
-
-void insert_sort(int *array, unsigned int n)
+void insert_sort(int *a, size_t n)
 {
     int i;
     int j;
     int temp;
 
-    for (i = 1; i < n; i++)
+    for (i = 0; i < n; i++)
     {
-        temp = *(array + i);
-        for (j = i; (j > 0) && (*(array + j -1) > temp); j--)
+        temp = a[i];
+        for (j = i; (j > 0) && (a[j-1] > temp); j--)
         {
-            *(array + j) = *(array + j -1);
+            a[j] = a[j-1];
         }
 
-        *(array + j) = temp;
+        a[j] = temp;
+    }
+}
+
+void bubble_sort(int *a, size_t n)
+{
+    int i;
+    int j;
+
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+        {
+            if (a[j] > a[j+1])
+            {
+                swap(&a[j], &a[j+1]);
+            }
+        }
     }
 
     return ;
@@ -113,5 +68,124 @@ void select_sort(int *array, unsigned int n)
     }
 
     return ;
+}
+
+void quick_sort(int *a, size_t left, size_t right)
+{
+    if (left >= right)
+        return ;
+
+    int i = left;
+    int j = right;
+    int key = a[i];
+
+    while (i < j)
+    {
+        while ((i < j) && (key <= a[j]))
+            j--;
+        a[i] = a[j];
+
+        while ((i < j) && (key >= a[i]))
+            i++;
+        a[j] = a[i];
+    }
+
+    a[i] = key;
+    quick_sort(a, left, i - 1);
+    quick_sort(a, i + 1, right);
+}
+
+int ascii_to_integer(const char *str)
+{
+    int value = 0;
+
+    while ((*str >= '0') && (*str <= '9'))
+    {
+        value *= 10;
+        value += *str++ - '0';
+    }
+
+    if (*str != '\0')
+        value = 0;
+
+    return value;
+}
+
+char *strrev(char *str)
+{
+    char *p = str;
+    char *q = str + strlen(str) - 1;
+
+    if (!str || !*str)
+        return NULL;
+
+    for (; p < q; p++, q--)
+    {
+        *p ^= *q;
+        *q ^= *p;
+        *p ^= *q;
+    }
+
+    return str;
+}
+
+char *integer_to_ascii(int n, char *s, int b)
+{
+    const char digits[] = "0123456789abcdefghijklmnopqrstuvwxyz";
+    int temp = n;
+    char *str_temp = s;
+
+    if (n < 0)
+        n = -n;
+
+    while (n > 0)
+    {
+        *s++ = digits[n % b];
+        n /= b;
+    }
+
+    if (temp < 0)
+        *s++ = '-';
+
+    *s = '\0';
+
+    return strrev(str_temp);
+}
+
+typedef void (*print_cb_t)(int n);
+
+void printWelcome(int n)
+{
+    printf("Welcome %d", n);
+    return ;
+}
+
+void printGoodbye(int n)
+{
+    printf("Goodbye %d", n);
+    return ;
+}
+
+void callback(print_cb_t print, int n)
+{
+    print(n);
+    printf("\r\n------------\r\n");
+}
+
+int main()
+{
+    int k[] = {9, 7, 1, 5, 3, 2};
+    quick_sort(k, 0, 5);
+
+    int i;
+    for (i = 0; i < 6; i++)
+        printf("%d ", k[i]);
+    printf("\n");
+
+    callback(printWelcome, 7);
+
+    callback(printGoodbye, 9);
+
+    exit(0);
 }
 
