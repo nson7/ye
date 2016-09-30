1485ca6e0dae72352881d892d4ff12ec7ab3e10d
nson7 <nson7@sina.com>
2016-09-30 15:35:10 +0800

    modified sort.c

--- sort.c
+++ sort.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <string.h>
 
 void swap(int *a, int *b)
@@ -154,6 +153,8 @@ char *integer_to_ascii(int n, char *s, int b)
 
 typedef void (*print_cb_t)(int n);
 
+print_cb_t a_print = NULL;
+
 void printWelcome(int n)
 {
     printf("Welcome %d", n);
@@ -166,16 +167,154 @@ void printGoodbye(int n)
     return ;
 }
 
-void callback(print_cb_t print, int n)
+void callback(print_cb_t print, int n,...)
 {
     print(n);
     printf("\r\n------------\r\n");
 }
 
+void hanoi(size_t n, char a, char b, char c)
+{
+    if (n > 0)
+    {
+        if (n == 1)
+        {
+            printf("%c -> %c\r\n", a, c);
+        }
+        else
+        {
+            hanoi(n-1, a, c, b);
+            printf("%c -> %c\r\n", a, c);
+            hanoi(n-1, b, a, c);
+        }
+    }
+}
+
+struct A
+{
+    int a:5;
+    int b:3;
+};
+
+struct C
+{
+    char *str_name;
+    unsigned n;
+};
+
+struct B
+{
+    struct C x;
+    int y;
+    char *b_name;
+};
+
+#define SWAP(a, b)    \
+    do {              \
+        a ^= b;           \
+        b ^= a;           \
+        a ^= b;           \
+    } while(0)
+
+int sn = 0;
+
+void permutation(char s[], int b, int e)
+{
+    if( (0 <= b) && (b <= e) )
+    {
+        if( b == e )
+        {
+            printf("%s\n", s);
+        }
+        else
+        {
+            int i = 0;
+
+            for(i=b; i<=e; i++)
+            {
+                char c = s[b];
+                s[b] = s[i];
+                s[i] = c;
+
+                permutation(s, b+1, e);
+
+                c = s[b];
+                s[b] = s[i];
+                s[i] = c;
+            }
+        }
+    }
+}
+
+int n = 100;
+
 int main()
 {
+    size_t m = 777;
+    int n = 999;
+
+    printf("n=%d\r\n", n);
+
+    printf("---\r\n");
+
+
+#define Smart
+#if !defined(Smart)
+    /* #ifdef Smart */
+    /* #ifndef Smart */
+    sn = 1;
+#else
+    sn = 7;
+#endif
+    printf("sn=%d\r\n", sn);
+    /* printf("Smart=%d\r\n", Smart); */
+
+#undef Smart
+
+#if !defined(Smart)
+    /* #ifdef Smart */
+    /* #ifndef Smart */
+    sn = 1;
+#else
+    sn = 7;
+#endif
+
+    printf("sn=%d\r\n", sn);
+
+    a_print = printWelcome;
+    if (a_print != NULL)
+        a_print(7);
+
+    a_print = printGoodbye;
+    if (a_print != NULL)
+        a_print(7);
+
+    SWAP(m, n);
+    printf("m=%d, n=%d\r\n", m, n);
+
+    struct B b =
+    {
+        .x =
+        {
+            .str_name = "nson7--",
+            .n = 77,
+        },
+        .y = 9,
+        .b_name = "ye-----",
+    };
+
+    printf("\r\nstr=%s, n=%d, y=%d, name=%s\r\n", b.x.str_name, b.x.n, b.y, b.b_name);
+
     int k[] = {9, 7, 1, 5, 3, 2};
     quick_sort(k, 0, 5);
+
+    char str[10] = "01234567";
+
+    struct A d;
+    memcpy(&d, str, sizeof(struct A));
+
+    printf("a=%d\r\n", d.a);
+    printf("b=%d\r\n", d.b);
 
     int i;
     for (i = 0; i < 6; i++)
@@ -185,6 +324,18 @@ int main()
     callback(printWelcome, 7);
 
     callback(printGoodbye, 9);
+
+    struct hanoita
+    {
+        char data[5];
+        short time[2];
+    };
+
+    printf("sizeof=%d\r\n", sizeof(struct hanoita));
+
+    int *N = malloc(sizeof(int)*5);
+
+    //hanoi(3, 'a', 'b', 'c');
 
     exit(0);
 }
